dee9fed03af63a34bb579080b78db991cf818bdc
Guilherme Barros <wingui3@gmail.com>
2021-09-17 11:34:54 -0300

    ♻️ Refactor fetchBitcoin

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,9 +1,9 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
-      btcPreco.innerText = 10 / bitcoin.BRL.sell.toFixed(8);
+      const btcPreco = document.querySelector(target);
+      btcPreco.textContent = 10 / bitcoin.BRL.sell.toFixed(8);
     })
     .catch((erro) => {
       console.log(Error(erro));
