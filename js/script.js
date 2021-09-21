cd6e89d057f4d510323904b3df347ecaf70f5932
Guilherme Barros <wingui3@gmail.com>
2021-09-21 12:50:32 -0300

    🐛 Correct url api

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
 const slide = new SlideNav('.slide', '.slide-wrapper');
